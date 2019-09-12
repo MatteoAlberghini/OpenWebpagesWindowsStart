a1a5a405459c95bab40f110bd0498df20dd225ab
Matteo Alberghini <mado.alberghini.main@gmail.com>
2019-09-12 14:31:52 +0200

    Add files via upload

---
+++ open-pages.py
@@ -0,0 +1,5 @@
+import webbrowser
+
+webbrowser.open_new('https://www.wunderlist.com/webapp#/lists/inbox')
+webbrowser.open('https://mail.google.com/mail/u/1/#inbox', new=2)
+webbrowser.open('https://trello.com/averagedoods/boards', new=2)
